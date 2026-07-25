de3b2c60ac70a69960e7c69b602e269f763dcd01
Mohd Zaid Bin Haneef <imzaid103@gmail.com>
2026-07-26 00:58:24 +0530

    Initial commit

---
+++ 1_main.py
@@ -0,0 +1,6 @@
+#Our first program gonna be----
+
+print("I am learning Python")
+
+## This is a simple code with which we are starting our journey of learning Python.
+## Just some formality for Shubh Aarambh, now lets begin....
---
+++ 2_Comments.py
@@ -0,0 +1,22 @@
+## Comments are something that are ignored by Python interpreter.
+## We use # for writing one-line comments in Python.
+## Multiline comments are not available in Python directly but we can achieve it by using Doc String """Multiline Comment""".
+
+
+# print("Hello this is an example of single line comment")
+
+"""
+
+This is an example of multiline comment. We call it Doc String.
+
+"""
+
+
+"""
+Some benefits of Comments:-
+1. Improve code readability - Make the code easier to understand.
+2. Explain complex logic - Help describe difficult parts of the code.
+3. Make maintenance easier - Easier to update or modify code later.
+4. Help in debugging - Developers can identify sections of code more easily.
+5. Improve teamwork - Other programmers can quickly understand the code.
+"""
